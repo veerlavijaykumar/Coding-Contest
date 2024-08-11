92bda40009af1beb92fff1f8c36e208fb9d973b2
veerlavijaykumar <160241484+veerlavijaykumar@users.noreply.github.com>
2024-08-11 13:50:52 +0530

    Update screenshots.js

--- models/screenshots.js
+++ models/screenshots.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const screenSchema = new Schema({
     name: { type: String, required: true },
-    screenshot: { type: Buffer, required: true }
+    screenshot: { type: Buffer, required: true },
+    programming_language:{type:String,required:true}
 }, { timestamps: true });
 
 const Screenshot = mongoose.model('Screenshot', screenSchema);
